bd91618d5b4bad93daa9e174d378cf7ef5b3f2f5
Ulas Can Cengiz <ulas@ulascengiz.com>
2019-12-12 02:00:13 +0300

    Update cached_network_image_provider.dart
    Update method signature for Flutter 1.12
    
    ImageStreamCompleter's load method signature has changed in the latest release of Flutter. This update reflects that change.

--- lib/src/cached_network_image_provider.dart
+++ lib/src/cached_network_image_provider.dart
@@ -39,7 +39,7 @@ class CachedNetworkImageProvider
   }
 
   @override
-  ImageStreamCompleter load(CachedNetworkImageProvider key) {
+  ImageStreamCompleter load(CachedNetworkImageProvider key, DecoderCallback decode) {
     return MultiFrameImageStreamCompleter(
       codec: _loadAsync(key),
       scale: key.scale,
