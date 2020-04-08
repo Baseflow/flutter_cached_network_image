abfe8d7cfc19967deefb9d141a20be4c8a3eef77
miyakeryo <ryo@miyake.id>
2020-04-08 14:43:20 +0900

    Fix flutter analyze

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:bitmap/bitmap.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blurhash/flutter_blurhash.dart';
