738f26f22bee82bb81b0b3b5ece8446e0f280cac
wen <alwjlola@gmail.com>
2024-05-29 01:26:48 +0800

    Replace Stream<ui.Codec> with Future<ui.Codec>

--- cached_network_image/lib/cached_network_image.dart
+++ cached_network_image/lib/cached_network_image.dart
@@ -7,4 +7,3 @@ export 'package:flutter_cache_manager/flutter_cache_manager.dart'
 
 export 'src/cached_image_widget.dart';
 export 'src/image_provider/cached_network_image_provider.dart';
-export 'src/image_provider/multi_image_stream_completer.dart';
